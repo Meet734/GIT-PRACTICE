010b775268a0c56e67ed1c5a9299a2622d2795f2
Meet <meetvaghasiya734@gmail.com>
2025-04-14 14:44:06 +0530

    Changed...

--- controller.js
+++ controller.js
@@ -1,1 +1,33 @@
 // This is controller js
+const getItems = (req, res) => {
+    res.send('Get all items');
+};
+
+const getItemById = (req, res) => {
+    const { id } = req.params;
+    res.send(`Get item with ID: ${id}`);
+};
+
+const createItem = (req, res) => {
+    const newItem = req.body;
+    res.send(`Create new item: ${JSON.stringify(newItem)}`);
+};
+
+const updateItem = (req, res) => {
+    const { id } = req.params;
+    const updatedItem = req.body;
+    res.send(`Update item with ID: ${id}, Data: ${JSON.stringify(updatedItem)}`);
+};
+
+const deleteItem = (req, res) => {
+    const { id } = req.params;
+    res.send(`Delete item with ID: ${id}`);
+};
+
+module.exports = {
+    getItems,
+    getItemById,
+    createItem,
+    updateItem,
+    deleteItem,
+};
